6e1bd72df17d306617dc5049c92a48adb32b18a0
oscar158 <2966476871@qq..com>
2020-11-09 21:25:26 +0800

    link Inerse

--- Sources/Link.h
+++ Sources/Link.h
@@ -3,24 +3,24 @@
 #include "DataStructType.h"
 
 template<typename T>
+class LinkNode
+{
+private:
+
+public:
+	LinkNode* next;
+	T mVal;
+	LinkNode(T val) :mVal(val), next(nullptr)
+	{
+
+	}
+};
+
+template<typename T>
 class Link 
 {
-
-	class LinkNode
-	{
-	private:
-		
-	public:
-		LinkNode* next;
-		T mVal;
-		LinkNode(T val):mVal(val), next(nullptr)
-		{
-
-		}
-	};
-
 private:
-	LinkNode* head;
+	LinkNode<T>* head;
 public:
 	Link():head(nullptr)
 	{
@@ -29,7 +29,7 @@ public:
 
 	void Push(T in)
 	{
-		LinkNode* node = new LinkNode(in);
+		LinkNode<T>* node = new LinkNode<T>(in);
 
 		if (head == nullptr)
 		{
@@ -42,21 +42,62 @@ public:
 		}
 	}
 
-	void DelNode(T in)
+	LinkNode<T>*  DelNode()
 	{
-
+		if (head == nullptr)
+			return nullptr;
+		
+		LinkNode<T>* node = nullptr;
+		node = head;
+		head = head->next;
+		return node;
 	}
+
+	//逆置链表
+	LinkNode<T>*  InerseNode()
+	{
+		if (head == nullptr)
+			return nullptr;
+
+		LinkNode<T>* pre = nullptr;
+		LinkNode<T>* next = nullptr;
+		while (head != nullptr)
+		{
+			//保留下一个节点
+			next = head->next;
+			//当前头结点指向前驱节点
+			head->next = pre;
+			//前驱节点和当前节点同时后移
+			pre = head;
+			head = next;
+		}
+		return pre;
+	}
+
+
 };
 
 void TestLink()
 {
 
 	Link<int> link;
+	link.Push(3);
 	link.Push(2);
-	link.Push(22);
-	cout << "" << endl;
+	link.Push(1);
 
+	LinkNode<int>* node = nullptr;
+	//node = link.InerseNode();
 
+	while (node)
+	{
+		cout << node->mVal << endl;
+		node = node->next;
+	}		
 
+	while (node = link.DelNode())
+	{
+		cout << node->mVal << endl;
+	}
 }
+
 RegisterStructTestFunction2(TestLink);
